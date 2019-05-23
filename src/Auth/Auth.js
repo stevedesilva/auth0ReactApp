2caa843e85f794e1b74062069d70a767aaa95fff
stevedesilva <steve.desilva@gmail.com>
2019-05-23 13:34:14 +0100

    react module

---
+++ src/Auth/Auth.js
@@ -0,0 +1,102 @@
+import auth0 from "auth0-js";
+
+const REDIRECT_ON_LOGIN = "redirect_on_login";
+
+export default class Auth {
+  constructor(history) {
+    // history = react router history
+    this.history = history;
+    this.userProfile = null;
+    this.requestedScopes = "openid profile email read:courses"; // permissions = use openid for auth;
+    this.auth0 = new auth0.WebAuth({
+      domain: process.env.REACT_APP_AUTH0_DOMAIN,
+      clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
+      redirectUri: process.env.REACT_APP_AUTH0_CALLBACK_URL,
+      audience: process.env.REACT_APP_AUTH0_AUDIENCE, // auth0 sends access token that is tied to a particular audience
+      responseType: "token id_token", // token = api access ;  id_token = openid connect
+      scope: this.requestedScopes
+    });
+  }
+
+  login = () => {
+    localStorage.setItem(
+      REDIRECT_ON_LOGIN,
+      JSON.stringify(this.history.location)
+    );
+    this.auth0.authorize();
+  };
+
+  handleAuthentication = () => {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult); // write parsed data to the session
+        const redirectLocation =
+          localStorage.getItem(REDIRECT_ON_LOGIN) === "undefined"
+            ? "/"
+            : JSON.parse(localStorage.getItem(REDIRECT_ON_LOGIN));
+        this.history.push(redirectLocation); // tells react router that we want to redirect to a new url
+      } else if (err) {
+        this.history.push("/");
+        alert(`Error: ${err.error}. Check the console for further details.`);
+        console.log(err);
+      }
+      localStorage.removeItem(REDIRECT_ON_LOGIN);
+    });
+  };
+
+  setSession = authResult => {
+    console.log(authResult);
+    // set the time that the access token will expire
+    const expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime()
+    );
+
+    const scopes = authResult.scope || this.requestedScopes || "";
+
+    localStorage.setItem("access_token", authResult.accessToken);
+    localStorage.setItem("id_token", authResult.idToken);
+    localStorage.setItem("expires_at", expiresAt);
+    localStorage.setItem("scopes", JSON.stringify(scopes));
+  };
+
+  isAuthenticated() {
+    const expiresAt = JSON.parse(localStorage.getItem("expires_at"));
+    return new Date().getTime() < expiresAt;
+  }
+
+  logout = () => {
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("id_token");
+    localStorage.removeItem("expires_at");
+    localStorage.removeItem("scopes");
+    // this.history.push("/"); // redirect to the home page
+    this.userProfile = null;
+    this.auth0.logout({
+      clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
+      returnTo: "http://localhost:3000"
+    });
+  };
+
+  getAccessToken = () => {
+    const accessToken = localStorage.getItem("access_token");
+    if (!accessToken) {
+      throw new Error("No access token found.");
+    }
+    return accessToken;
+  };
+
+  getProfile = cb => {
+    if (this.userProfile) return cb(this.userProfile);
+    this.auth0.client.userInfo(this.getAccessToken(), (err, profile) => {
+      if (profile) this.userProfile = profile;
+      cb(profile, err);
+    });
+  };
+
+  userHasScopes(scopes) {
+    const grantedScopes = (
+      JSON.parse(localStorage.getItem("scopes")) || ""
+    ).split(" ");
+    return scopes.every(scope => grantedScopes.includes(scope));
+  }
+}
---
+++ src/Private.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class Private extends Component {
+  state = {
+    message: ""
+  };
+
+  componentDidMount() {
+    fetch("/private", {
+      headers: { Authorization: `Bearer ${this.props.auth.getAccessToken()}` }
+    })
+      .then(response => {
+        if (response.ok) return response.json();
+        throw Error("Network response was not ok!");
+      })
+      .then(response => this.setState({ message: response.message }))
+      .catch(error => this.setState({ message: error.message }));
+  }
+  render() {
+    return <p> {this.state.message} </p>;
+  }
+}
+
+export default Private;
